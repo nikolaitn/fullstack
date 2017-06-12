cac168407d4fcfeb8880b27988f3e7f85442110a
Nikolay Ninov <nikolaitn@gmail.com>
2017-06-12 22:08:45 +0300

    Initial project commit

---
+++ src/main/java/com/nninov/DevOpsFullstackApplication.java
@@ -0,0 +1,12 @@
+package com.nninov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsFullstackApplication.class, args);
+	}
+}
